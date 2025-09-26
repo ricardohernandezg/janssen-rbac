6c8f5398acab0e461934392872da08f9a6e00328
Ricardo Hernández <ricardohernandezg@gmail.com>
2025-09-25 21:58:53 -0400

    add event register to App

--- src/Helpers/GlobalRbacFunctions.php
+++ src/Helpers/GlobalRbacFunctions.php
@@ -4,15 +4,15 @@ namespace Janssen\Helpers;
 
 use Janssen\App;
 use Janssen\Engine\Config;
+use Janssen\Engine\Event;
 use Exception;
 
 /**
  * here we should check that 
- * - we are running inside Janssen core,
- * - we have the engine settings or independent file named rbac in Config namespace
- * - we have redis enabled
- * - we have the database for persistence
- * - we have the tables scaffolding or the settings enabled
+ * x we are running inside Janssen core,
+ * x we have the engine settings or independent file named rbac in Config namespace
+ * x we have redis enabled
+ * - we have the database for persistence and tables scaffolding 
  */
 
 if (!class_exists('Janssen\App')) {
@@ -21,13 +21,20 @@ if (!class_exists('Janssen\App')) {
 
 } 
 
-// load rbac config
-$rbac_conf_candidate = App::getPathCandidate('rbac');
-Config::setAll((is_file($rbac_conf_candidate)) ? (include $rbac_conf_candidate) : []);
-self::$engine_config = Config::get();
-
 if (!extension_loaded('redis')) {
 
     throw New \Exception('This package needs Redis to work', 500);
 
 } 
+
+Event::listen('app.afterinit', function(){
+    // set configuration from rbac file
+    $rbac_conf_candidate = App::getPathCandidate('rbac');
+    Config::append((is_file($rbac_conf_candidate)) ? (include $rbac_conf_candidate) : []);
+
+    // check the database scaffolding
+
+
+});
+
+
